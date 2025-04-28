18a135a9fcb7f4a2b133e914c4c3e2a1de75cd1e
Varsha <varsha.pandian@gmail.com>
2025-04-28 12:16:32 +0530

    Day 2 - Moves Zeros to End

---
+++ movesZerosToEnd.java
@@ -0,0 +1,13 @@
+class Solution {
+    void pushZerosToEnd(int[] arr){
+        int nonZeroIndex=0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] != 0){
+                arr[nonZeroIndex++]=arr[i];
+            }
+        }
+        while(nonZeroIndex<arr.length){
+            arr[nonZeroIndex++]=0;
+        }
+    }
+}
